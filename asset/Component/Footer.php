8e3fad73b45adcac757ac8fca6fede071640d6c5
Océane Gontier <108407770+Goceane149@users.noreply.github.com>
2023-05-20 18:05:57 +0200

    footer.php

---
+++ asset/Component/Footer.php
@@ -0,0 +1,9 @@
+<footer class="fixed-bottom">
+    <div class="container-fluid">
+        <div class="row text-white">
+            <div class="col align-middle text-left "><img class="img-fluid" src="asset/img/logofooter.png" alt="Logo US Cousolre"></div>
+            <div class="col d-none d-lg-block text-center">© LS Custom | Arkana City</div>
+            <div class="col  align-middle text-end text-nowrap"><a href="mentions.php">Mentions légales</a></div>
+        </div>
+    </div>
+</footer>
